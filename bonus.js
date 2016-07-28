fd063a77bf33fa6f2f27083e884d398b072c74cd
spflow <sean.pascoe@gmail.com>
2016-07-28 09:34:21 -0600

    drills complete

---
+++ bonus.js
@@ -0,0 +1,250 @@
+var numberEl = document.getElementById('number');
+var numeralEl = document.getElementById('roman-numeral')
+
+function convertRoman() {
+  var num = numberEl.value;
+  var firstDigit = num.slice(-1);
+  var secondDigit = num.slice(-2,-1);
+  var thirdDigit = num.slice(-3,-2);
+  var fourthDigit = num.slice(-4,-3);
+  var fifthDigit = num.slice(-5,-4);
+  var sixthDigit = num.slice(-6,-5);
+
+  var romanNumeral = convSixthDigit(sixthDigit) + convFifthDigit(fifthDigit) + convFourthDigit(fourthDigit) + convThirdDigit(thirdDigit) + convSecondDigit(secondDigit) + convFirstDigit(firstDigit);
+
+  numeralEl.innerHTML = romanNumeral;
+}
+
+function convFirstDigit(num) {
+  if (num) {
+    switch (parseInt(num)) {
+      case 0:
+        return "";
+        break;
+      case 1:
+        return "I";
+        break;
+      case 2:
+        return "II";
+        break;
+      case 3:
+        return "III";
+        break;
+      case 4:
+        return "IV";
+        break;
+      case 5:
+        return "V";
+        break;
+      case 6:
+        return "VI";
+        break;
+      case 7:
+        return "VII";
+        break;
+      case 8:
+        return "VIII";
+        break;
+      case 9:
+        return "IX";
+        break;
+    }
+  } else {
+    return "";
+  }
+}
+
+function convSecondDigit(num) {
+  if (num) {
+    switch (parseInt(num)) {
+      case 0:
+        return "";
+        break;
+      case 1:
+        return "X";
+        break;
+      case 2:
+        return "XX";
+        break;
+      case 3:
+        return "XXX";
+        break;
+      case 4:
+        return "XL";
+        break;
+      case 5:
+        return "L";
+        break;
+      case 6:
+        return "LX";
+        break;
+      case 7:
+        return "LXX";
+        break;
+      case 8:
+        return "LXXX";
+        break;
+      case 9:
+        return "XC";
+        break;
+    }
+  } else {
+    return "";
+  }
+}
+
+function convThirdDigit(num) {
+  if (num) {
+    switch (parseInt(num)) {
+      case 0:
+        return "";
+        break;
+      case 1:
+        return "C";
+        break;
+      case 2:
+        return "CC";
+        break;
+      case 3:
+        return "CCC";
+        break;
+      case 4:
+        return "CD";
+        break;
+      case 5:
+        return "D";
+        break;
+      case 6:
+        return "DC";
+        break;
+      case 7:
+        return "DCC";
+        break;
+      case 8:
+        return "DCCC";
+        break;
+      case 9:
+        return "CM";
+        break;
+    }
+  } else {
+    return "";
+  }
+}
+
+function convFourthDigit(num) {
+  if (num) {
+    switch (parseInt(num)) {
+      case 0:
+        return "";
+        break;
+      case 1:
+        return "M";
+        break;
+      case 2:
+        return "MM";
+        break;
+      case 3:
+        return "MMM";
+        break;
+      case 4:
+        return "M<span>V</span>";
+        break;
+      case 5:
+        return "<span>V</span>";
+        break;
+      case 6:
+        return "<span>VI</span>";
+        break;
+      case 7:
+        return "<span>VII</span>";
+        break;
+      case 8:
+        return "<span>VIII</span>";
+        break;
+      case 9:
+        return "M<span>X</span>";
+        break;
+    }
+  } else {
+    return "";
+  }
+}
+
+function convFifthDigit(num) {
+  if (num) {
+    switch (parseInt(num)) {
+      case 0:
+        return "";
+        break;
+      case 1:
+        return "<span>X</span>";
+        break;
+      case 2:
+        return "<span>XX</span>";
+        break;
+      case 3:
+        return "<span>XXX</span>";
+        break;
+      case 4:
+        return "M<span>XL</span>";
+        break;
+      case 5:
+        return "<span>L</span>";
+        break;
+      case 6:
+        return "<span>LX</span>";
+        break;
+      case 7:
+        return "<span>LXX</span>";
+        break;
+      case 8:
+        return "<span>LXXX</span>";
+        break;
+      case 9:
+        return "<span>XC</span>";
+        break;
+    }
+  } else {
+    return "";
+  }
+}
+
+function convSixthDigit(num) {
+  if (num) {
+    switch (parseInt(num)) {
+      case 0:
+        return "";
+        break;
+      case 1:
+        return "<span>C</span>";
+        break;
+      case 2:
+        return "<span>CC</span>";
+        break;
+      case 3:
+        return "<span>CCC</span>";
+        break;
+      case 4:
+        return "M<span>CD</span>";
+        break;
+      case 5:
+        return "<span>D</span>";
+        break;
+      case 6:
+        return "<span>DC</span>";
+        break;
+      case 7:
+        return "<span>DCC</span>";
+        break;
+      case 8:
+        return "<span>DCCC</span>";
+        break;
+      case 9:
+        return "<span>CM</span>";
+        break;
+    }
+  } else {
+    return "";
+  }
+}
---
+++ drills.js
@@ -0,0 +1,137 @@
+//Drill 1
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+document.getElementById('dr1').innerHTML = "The sum of 1 and 5 is " + sum(1,5);
+
+//Drill 2
+var awesomeArray = [1,4,5,7,3,58,6,2,3,5];
+
+function sumArray(arr) {
+  var sum = 0;
+  for(var i = 0; i<arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+document.getElementById('dr2').innerHTML = `The sum of the numbers in "awesomeArray" is ${sumArray(awesomeArray)}. Here is the array: ${awesomeArray}`;
+
+//Drill 3
+var fTemp = 212;
+var cTemp = fToC(fTemp);
+
+function fToC(f) {
+  return (f - 32) * (5/9);
+}
+
+document.getElementById('dr3').innerHTML = `The F temp ${fTemp}&deg; is equal to ${cTemp}&deg; in C`;
+
+
+//Drill 4
+var string = "Hi, my name is Johnny Appleseed!";
+var vowels = ['a','e','i','o','u'];
+
+function vowelCount(str) {
+  var vCount = 0;
+  for(let i = 0; i < str.length; i++) {
+    if (vowelCheck(str[i])) {
+      vCount += 1;
+    }
+  }
+  return vCount;
+}
+
+function vowelCheck(char) {
+  for(let i = 0; i < vowels.length; i++) {
+    if(char.toLowerCase() === vowels[i].toLowerCase()) {
+      return true;
+    }
+  }
+  return false;
+}
+
+document.getElementById('dr4').innerHTML = `The number of vowels in "${string}" is ${vowelCount(string)}`;
+
+
+
+// Drill 5
+var rollArray = [];
+
+function randomDie(){
+  return Math.floor((Math.random()*6) + 1)
+};
+
+(function rollTheDice() {
+  rollArray.unshift(randomDie());
+  rollArray.unshift(randomDie());
+})();
+
+document.getElementById('dr5').innerHTML = `You just rolled a ${rollArray}`;
+
+
+// Drill 6
+
+// awesomeArray (see above)
+
+function avgOfArray(arr) {
+  var sum = 0;
+  for(var i = 0; i<arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+
+document.getElementById('dr6').innerHTML = `The average of the numbers in "awesomeArray" (see drill 2) is ${avgOfArray(awesomeArray)}`;
+
+
+// Drill 7
+
+// (function fizzBuzz() {
+//   for(var i = 1; i<=100; i++) {
+//     if ( i % 3 === 0 && i % 5 === 0 ) {
+//       console.log('FizzBuzz');
+//     } else if ( i % 3 === 0 ) {
+//       console.log('Fizz');
+//     } else if ( i % 5 === 0 ) {
+//       console.log('Buzz');
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// })();
+
+document.getElementById('dr7').innerHTML = `See console!`;
+
+
+// Drill 8
+var number = 7;
+
+function factorial(num) {
+  for(var i = num-1; i > 0; i--) {
+    num *= i;
+  }
+  return num;
+}
+
+document.getElementById('dr8').innerHTML = `The factorial of ${number} is ${factorial(number)}`;
+
+
+//drill 9
+
+function fib(num) {
+  var a = 0;
+  var b = 1;
+  var c;
+
+  for(var i = 0; i < num; i++) {
+    //console.log(a);
+    c = a + b
+    a = b
+    b = c
+  }
+  return a;
+}
+
+document.getElementById('dr9').innerHTML = `The number that is at the Fibonacci sequence index of 45 is ${fib(45)}`;
